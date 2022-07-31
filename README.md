5520f2251a2575331ba7c2d5615c4565a528342f
Megarion <andy181209x@gmail.com>
2022-07-31 21:50:24 +0700

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# NoYoutubeShorts
+This Chrome extension is for personal use <br> [Installation instructions](https://gist.github.com/Megarion/f5af750947ec2b9309b888d1c8f44238)
+
+### Features
+- [x] Redirect when clicked on a Youtube Short
+- [ ] Automatically enable looping
---
+++ src/script.js
@@ -0,0 +1,26 @@
+let lastUrl = location.href; 
+new MutationObserver(() => {
+    const url = location.href;
+    if (url !== lastUrl) {
+        lastUrl = url;
+        onUrlChange();
+    }
+}).observe(document, {subtree: true, childList: true});
+
+function onUrlChange() {
+    start();
+}
+
+function autoRedirect(){
+    let redirect = lastUrl;
+    window.location.replace(redirect.replace(/shorts\//, 'watch?v='));
+}
+
+function start(){
+    if (lastUrl.startsWith("https://www.youtube.com/shorts")) {
+        clearInterval(check);
+        autoRedirect();
+    }
+}
+
+start();
